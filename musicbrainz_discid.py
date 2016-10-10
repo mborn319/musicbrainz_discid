957009676b47c0b2ec1c19addc13d49174e78c6f
Michael Born <mborn319@gmail.com>
2016-10-10 15:25:19 -0400

    Added JSON option, better usage info

--- musicbrainz_discid.py
+++ musicbrainz_discid.py
@@ -4,51 +4,95 @@
 
 import os
 import sys
+import json
 import discid
 import musicbrainzngs as ws
 
-if len(sys.argv) > 2 or (len(sys.argv) == 2 and sys.argv[1] == "-h"):
-	print("usage: {0:s} [device]".format(os.path.basename(sys.argv[0])))
-	exit(1)
-elif len(sys.argv) == 2:
-	device = sys.argv[1]
+output="NORMAL"
+device=discid.get_default_device()
+
+def usage():
+    fname = os.path.basename(sys.argv[0]) 
+    print("usage: {0:s} [DEVICE] [OPTION]".format(fname))
+    print("[DEVICE]   Example: /dev/cdrom")
+    print("-h       print help information and exit")
+    print("-j       return results in JSON format")
+
+def parseOpts():
+    global output, device
+    
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            if arg == "-h":
+                usage()
+                exit(1)
+            elif arg == "-j":
+                output="JSON"
+            else:
+                device=arg
+
+def getDisc(device):
+    return discid.read(device=device)
+
+def getReleases(disc):
+    ws.set_useragent("musicbrainz_discid","0.2")
+    includeVars = ["artists", "recordings", "labels"]
+    return ws.get_releases_by_discid(disc.id,toc=disc.toc_string,cdstubs=False,includes=includeVars)
+
+# read options, return usage or option variables
+parseOpts()
+
+if output != "JSON":
+    print("Device: " + device)
+
+# get the disc table of contents
+disc = getDisc(device)
+
+if output != "JSON":
+    print("Disc ID: " + disc.id)
+    print("TOC:     " + disc.toc_string)
+
+# get the artist/album/track info for the disc
+r = getReleases(disc)
+
+if "disc" in r and r["disc"]["release-count"] > 0:
+    if output != "JSON":
+        print("Disc ID found")
+    releases = r["disc"]["release-list"]
+elif "release-list" in r and len(r["release-list"]) > 0:
+    if output != "JSON":
+        print("TOC search succeeded")
+    releases = r["disc"]["release-list"]
 else:
-	device = discid.get_default_device();
+    if output != "JSON":
+        print("Release not found")
+    else:
+        print('{"success":false,"message":"Release not found"}')
+    exit(1)
 
-print("Device: " + device)
+    
 
-disc = discid.read(device=device)
-print("Disc ID: " + disc.id)
-print("TOC:     " + disc.toc_string)
-
-ws.set_useragent("musicbrainz_discid", "0.1")
-r = ws.get_releases_by_discid(disc.id, toc=disc.toc_string, cdstubs=False, includes=["artists", "recordings", "labels"])
-
-if 'disc' in r and r['disc']['release-count'] > 0:
-	print("Disc ID found")
-	releases = r['disc']['release-list']
-elif 'release-list' in r and len(r['release-list']) > 0:
-	print("TOC search succeeded")
-	releases = r['release-list']
+if output == "JSON":
+    # print JSON
+    print(json.dumps(releases))
 else:
-	print("Release not found")
-	exit(1)
+    # print plain text
 
-rnum = 1
-for x in releases:
-	print("Release " + str(rnum) + ":")
-	print("  ID:     " + x['id'])
-	print("  URL:    http://musicbrainz.org/release/" + x['id'])
-	print("  Title:  " + x['title'] + " ["
-		+ (x['release-event-list'][0]['date'] if 'release-event-list' in x and 'date' in x['release-event-list'][0] else "<no date>")
-		+ ", " + (x['country'] if 'country' in x else "<no country>")
-		+ ", " + (x['label-info-list'][0]['label']['name'] if x['label-info-count'] > 0 else "<no label>") + "]")
-	print("  Artist: " + x['artist-credit'][0]['artist']['name'])
-	print("  Tracks:")
-	for x in x['medium-list']:
-		print("    " + (x['format'] if 'format' in x else "Medium") + " " + str(x['position']) + ":")
-		tnum = 1
-		for x in x['track-list']:
-			print("      " + "{0:02d} ".format(tnum) + x['recording']['title'])
-			tnum += 1
-	rnum += 1
+    rnum = 1
+    for x in releases:
+        print("Release " + str(rnum) + ":")
+        print("  ID:     " + x['id'])
+        print("  URL:    http://musicbrainz.org/release/" + x['id'])
+        print("  Title:  " + x['title'] + " ["
+                + (x['release-event-list'][0]['date'] if 'release-event-list' in x and 'date' in x['release-event-list'][0] else "<no date>")
+                + ", " + (x['country'] if 'country' in x else "<no country>")
+                + ", " + (x['label-info-list'][0]['label']['name'] if x['label-info-count'] > 0 else "<no label>") + "]")
+        print("  Artist: " + x['artist-credit'][0]['artist']['name'])
+        print("  Tracks:")
+        for x in x['medium-list']:
+                print("    " + (x['format'] if 'format' in x else "Medium") + " " + str(x['position']) + ":")
+                tnum = 1
+                for x in x['track-list']:
+                        print("      " + "{0:02d} ".format(tnum) + x['recording']['title'])
+                        tnum += 1
+        rnum += 1
